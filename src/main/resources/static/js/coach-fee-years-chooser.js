cf7a79ab5ee9e4f79debd1d51dbc53aa722cf71b
DK <doriancfc@gmail.com>
2019-10-30 20:49:21 +0100

    Change layout of fees page

--- src/main/resources/static/js/coach-fee-years-chooser.js
+++ src/main/resources/static/js/coach-fee-years-chooser.js
@@ -1,14 +1,17 @@
 var selected_year = parseInt((window.location.href).split("/").pop().split("?")[0]);
 var player_id = parseInt((window.location.href).split("/").slice(-2)[0]);
 var div_content = "";
-if(selected_year-1 >= 2019) {
-    var prev_year = selected_year-1;
-    div_content += "<a href=\"/coach/players-fees/" + player_id + "/" + prev_year + "\" style=\"margin-right: 250px;color: #2d3447;\"><<< " + prev_year + "</a>";
+var style = "color: #2d3447;";
+if(selected_year-1 < 2019) {
+    style += " visibility: hidden;";
 }
-if(selected_year <= new Date().getFullYear()) {
-    var next_year = selected_year+1;
-    div_content += "<a href=\"/coach/players-fees/" + player_id + "/" + next_year + "\" style=\"margin-left: 250px;color: #2d3447;\">" + next_year + " >>></a>";
+div_content += "<a href=\"/coach/players-fees/" + player_id + "/" + (selected_year-1) + "\" style=\" " + style + "\"><<<</a>";
+div_content += "<span style=\"font-size:20px; margin:0px 30px;\">" + selected_year + "</span>";
+style = "color: #2d3447;";
+if(selected_year > new Date().getFullYear()) {
+    style += " visibility: hidden;";
 }
+div_content += "<a href=\"/coach/players-fees/" + player_id + "/" + (selected_year+1) + "\" style=\" " + style + "\">>>></a>";
 document.getElementById("feeYearsDiv").innerHTML = div_content;
 document.feesForm.action = getActionYear();
 
